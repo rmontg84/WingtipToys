eee3135fc7ca78219fd7fe1a29fae74377e7a9c4
rmontg84 <rmontg84@gmail.com>
2015-01-22 09:57:01 -0600

    Wingtip Toys
    ASP.NET

---
+++ WingtipToys/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Microsoft.Owin.Security;
+
+[assembly: OwinStartupAttribute(typeof(WingtipToys.Startup))]
+namespace WingtipToys
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigAuth(app);
+        }
+        public void ConfigAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Login")
+            });
+        }
+    }
+}
